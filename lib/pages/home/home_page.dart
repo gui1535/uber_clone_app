cb4d3bbbc0cb6e728f14fc6c8f3da6536f6610c4
gui1535 <guilherme.araujo1535@gmail.com>
2023-03-15 01:10:54 -0300

    Melhorando codigos, dividindo por functions

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -7,69 +7,81 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [Colors.black, Colors.black87])),
+          decoration: _decorationGradientContainer(),
           child: Column(
             children: [
-              ClipPath(
-                clipper: DiagonalPathClipperTwo(),
-                child: Container(
-                  color: Colors.white,
-                  height: MediaQuery.of(context).size.height * 0.3,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Image.asset(
-                        'assets/img/logo_app.png',
-                        width: 150,
-                        height: 100,
-                      ),
-                      Text(
-                        'Fácil e rápido',
-                        style: TextStyle(
-                            fontFamily: 'Pacifico',
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ),
-                ),
-              ),
+              _bannerApp(context),
               SizedBox(height: 50),
-              Text(
-                'SELECIONE SUA FUNÇÃO',
-                style: TextStyle(
-                    fontFamily: 'OneDay', fontSize: 20, color: Colors.white),
-              ),
+              _textSelectYourEnter(),
               SizedBox(height: 30),
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/img/pasajero.png'),
-                radius: 50,
-                backgroundColor: Colors.grey[900],
-              ),
+              _imageTypeUser('assets/img/pasajero.png'),
               SizedBox(height: 10),
-              Text(
-                'Cliente',
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
+              _textTypeUser('Cliente'),
               SizedBox(height: 30),
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/img/driver.png'),
-                radius: 50,
-                backgroundColor: Colors.grey[900],
-              ),
+              _imageTypeUser('assets/img/driver.png'),
               SizedBox(height: 10),
-              Text(
-                'Condutor',
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
+              _textTypeUser('Condutor')
             ],
           ),
         ),
       ),
+    );
+  }
+
+  BoxDecoration _decorationGradientContainer() {
+    return BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Colors.black, Colors.black87]));
+  }
+
+  Widget _bannerApp(BuildContext context) {
+    return ClipPath(
+      clipper: DiagonalPathClipperTwo(),
+      child: Container(
+        color: Colors.white,
+        height: MediaQuery.of(context).size.height * 0.3,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(
+              'assets/img/logo_app.png',
+              width: 150,
+              height: 100,
+            ),
+            Text(
+              'Fácil e rápido',
+              style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _textSelectYourEnter() {
+    return Text(
+      'Como deseja entrar?',
+      style: TextStyle(fontFamily: 'OneDay', fontSize: 20, color: Colors.white),
+    );
+  }
+
+  Widget _imageTypeUser(String asset) {
+    return CircleAvatar(
+      backgroundImage: AssetImage(asset),
+      radius: 50,
+      backgroundColor: Colors.grey[900],
+    );
+  }
+
+  Widget _textTypeUser(String text) {
+    return Text(
+      text,
+      style: TextStyle(color: Colors.white, fontSize: 16),
     );
   }
 }
