2bdb0f48a9090884c73840bdafacc2b4f088fb53
gui1535 <guilherme.araujo1535@gmail.com>
2023-03-15 00:11:35 -0300

    Estrutura da home

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -4,22 +4,40 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Uber Clone'),
-      // ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Image.asset(
-                'assets/img/logo_app.png',
-                width: 150,
-                height: 100,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(
+                  'assets/img/logo_app.png',
+                  width: 150,
+                  height: 100,
                 ),
-              Text('Fácil e rápido')
-            ],
-          )
-        ],
+                Text('Fácil e rápido')
+              ],
+            ),
+            SizedBox(height: 50),
+            Text('SELECIONE SUA FUNÇÃO'),
+            SizedBox(height: 30),
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/img/pasajero.png'),
+              radius: 50,
+              backgroundColor: Colors.black,
+            ),
+            SizedBox(height: 10),
+            Text('Cliente'),
+            SizedBox(height: 30),
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/img/driver.png'),
+              radius: 50,
+              backgroundColor: Colors.black,
+            ),
+            SizedBox(height: 10),
+            Text('Condutor')
+          ],
+        ),
       ),
     );
   }
