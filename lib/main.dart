0a49650b35d92cbd9ba121b12342cae9936d4dbb
gui1535 <guilherme.araujo1535@gmail.com>
2023-04-16 19:10:14 -0300

    Definindo primary color theme do aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:uber_clone/pages/home/home_page.dart';
 import 'package:uber_clone/pages/login/login_page.dart';
+import 'package:uber_clone/utils/colors.dart' as utils;
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +21,15 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       title: 'Uber Clone',
       initialRoute: 'home',
-      theme: ThemeData(fontFamily: 'NimbusSans'),
+      theme: ThemeData(
+        fontFamily: 'NimbusSans',
+        // primaryColor: utils.Colors.uberCloneColor,
+        primarySwatch: utils.Colors.primarySwatch,
+        appBarTheme: AppBarTheme(
+          elevation: 0,
+          backgroundColor: utils.Colors.uberCloneColor,
+        ),
+      ),
       routes: {
         'home': (BuildContext context) => HomePage(),
         'login': (BuildContext context) => LoginPage()
